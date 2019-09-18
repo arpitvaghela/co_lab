46680446e7aeba669b527682abcf61409233bfca
student <student@AU-SEAS-L117-02.ahduni.local>
2019-09-18 14:49:56 +0530

    working on lab_6

--- Tue_03_MIPS/JC_block/JC_block.v
+++ Tue_03_MIPS/JC_block/JC_block.v
@@ -56,19 +56,23 @@ module JC_block(
     assign JNZ = ~op[5] & op[4] & op[3] & op[2] & op[1] & op[0] ; // jump without zero flag
     // unconditional jump
     assign JMP = ~op[5] & op[4] & op[3] & ~op[2] & ~op[1] & ~op[0] ;
-    // return 
+    // return statement
     assign RET = ~op[5] & op[4] & ~op[3] & ~op[2] & ~op[1] & ~op[0] ;
 
     // hold flag values
     assign flag_mux_1 = (F2) ? flag_ex : flag_reg ;
     assign flag_mux_2 = (RET) ? flag_reg : flag_ex ;
+    
     // hold the address  
     assign nxt_address = current_address + 1;
     assign current_address_mux = (interrupt)? nxt_address : current_address_reg ;
+    
     // set to f000 one clk after interrupt else jmp_address 
     assign jmp_address_mux = (F1) ? 'hf000 : jmp_address_pm;
+    
     // select between stored current_address and jum_locations 
     assign jmp_loc = (RET) ? current_address_reg : jmp_address_mux ;
+    
     // pc_mux_sel on jump instructions
     assign pc_mux_sel = ( JV & flag_mux_2[0] ) | ( ~flag_mux_2[0] & JNV ) | ( JZ & flag_mux_2[1] ) | ( ~flag_mux_2[1] & JNZ ) | JMP | RET | F1 ;
 
